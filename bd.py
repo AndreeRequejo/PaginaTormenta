7497e164129e65281a3713fea2ec3fac8234d683
AndreeRequejo <andree09requejo@gmail.com>
2023-11-03 11:08:33 -0500

    V2 3/11/2023

--- bd.py
+++ bd.py
@@ -4,5 +4,5 @@ def obtener_conexion():
     return pymysql.connect(host='127.0.0.1',
                                 port=3306,
                                 user='root',
-                                password='1234',
+                                password='',
                                 db='bdproyectofinal')
